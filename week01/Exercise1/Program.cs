d7009a497cf6a98a662193b0e9707c9b0469a80a
slplaine <simoneleca@gmail.com>
2026-01-13 14:49:21 -0300

    exercicio 1 concluido

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // First name entry
+        Console.Write("What is your first name? ");
+         string? first = Console.ReadLine(); 
+         // Last name entry
+         Console.Write("What is your last name? "); 
+         string? last = Console.ReadLine();
+          // Output formatted name
+          Console.WriteLine($"Your name is {last}, {first} {last}");
     }
 }
