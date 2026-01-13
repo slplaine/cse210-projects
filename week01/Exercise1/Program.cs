629297284398dfe72d4e8cfaa18cebd366ae6dc2
slplaine <simoneleca@gmail.com>
2026-01-13 14:50:12 -0300

    exercicio1 concluido

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -12,6 +12,6 @@ class Program
          Console.Write("What is your last name? "); 
          string? last = Console.ReadLine();
           // Output formatted name
-          Console.WriteLine($"Your name is {last}, {first} {last}");
+          Console.WriteLine($"Your name is {last}, {first} {last}"); 
     }
 }
