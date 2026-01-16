b1592f90bcb0dd8a5d274fd6f727f48aca100d2c
slplaine <simoneleca@gmail.com>
2026-01-16 11:01:51 -0300

    atividade concluida

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2016;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2024;
+        job2._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "Simone Plaine";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+        // Checking if I can access the title of the first job
+         Console.WriteLine("First work (via dot notation): " + myResume._jobs[0]._jobTitle);
     }
 }
