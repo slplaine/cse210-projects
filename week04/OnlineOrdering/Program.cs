b779f9b564990e21ee4ca09b3345da217b3b73c4
slplaine <simoneleca@gmail.com>
2026-02-04 00:02:36 -0300

    making improvements

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,15 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Address address1 = new Address("123 Main St", "New York", "NY", "USA");
-        Customer customer1 = new Customer("John Smith", address1);
+        Address address1 = new Address("389 Coelho Neto", "Joinville", "SC", "Brazil");
+        Customer customer1 = new Customer("Athos PLaine", address1);
 
         Order order1 = new Order(customer1);
         order1.AddProduct(new Product("Laptop", "P1001", 800, 1));
         order1.AddProduct(new Product("Mouse", "P1002", 25, 2));
 
-        Address address2 = new Address("456 Queen St", "Toronto", "ON", "Canada");
-        Customer customer2 = new Customer("Emily Brown", address2);
+        Address address2 = new Address("845 Oak Avenue", "Denver", "CO", "USA");
+        Customer customer2 = new Customer("Emily Smith", address2);
 
         Order order2 = new Order(customer2);
         order2.AddProduct(new Product("Phone", "P2001", 600, 1));
