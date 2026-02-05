18b26af6f26651c2beb52f577b14066e09d23c67
slplaine <simoneleca@gmail.com>
2026-02-05 03:52:43 -0300

    activity completed

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Create a base "Assignment" object
+        Assignment a1 = new Assignment("Guilherme Plaine", "Alfabetization");
+        Console.WriteLine(a1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment a2 = new MathAssignment("Mirela Plaine", "Fractions", "7", "8-10");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Mirela Plaine", "Brasilian History", "The Empire of Pedro II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
